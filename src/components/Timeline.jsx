79ecf0b5f990a84a7e927fac0a4f159a486252ad
eehkani <82057076+eehkani@users.noreply.github.com>
2021-09-12 19:21:45 +0900

    Comment & Timeline.jsx
    input欄にIdを追加しました。
    それに伴い、comment.jsxとTimeline.jsxを変更しました

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -5,18 +5,20 @@ import React, {useState} from "react";
 
 const Timeline = () => {
     
-    const [comments, setComments] = useState([{username: "Kazuma", txt: "柿食えば 鐘が鳴るなり 法隆寺", rep: []}])
+    const [comments, setComments] = useState([{username: "Kazuma", userId: "K_azuma",txt: "柿食えば 鐘が鳴るなり 法隆寺", rep: []}])
     const [username, setUsername] = useState("")
+    const [userId, setUserId] = useState("")
     const [lineOne, setLineOne] = useState("")
     const [lineTwo, setLineTwo] = useState("")
     const [lineThree, setLineThree] = useState("")
 
     const handleComments = () => {
         const txt = lineOne + " "+ lineTwo + " "+ lineThree;
-        if(username && txt.trim()){
-            const newComment = {username: username, txt: txt, rep: []}
+        if(username && userId && txt.trim()){
+            const newComment = {username: username, userId: userId, txt: txt, rep: []}
             setComments([...comments, newComment])
             setUsername("")
+            setUserId("")
             setLineOne("")
             setLineTwo("")
             setLineThree("")
@@ -61,6 +63,7 @@ const Timeline = () => {
             <header className="timeline_title"><h1>ホーム</h1></header>
             <div className="timeline_postWrapper">
                 <input className="timeline_post" type="text" placeholder="username" value={username} onChange={e=> setUsername(e.target.value)}/>
+                <input className="timeline_post" type="text" placeholder="user ID" value={userId} onChange={e=> setUserId(e.target.value)}/>
                 <input className="timeline_post" type="text" placeholder="5" value={lineOne} onChange={e=> setLineOne(e.target.value)}/>
                 <input className="timeline_post" type="text" placeholder="7" value={lineTwo} onChange={e=> setLineTwo(e.target.value)}/>
                 <input className="timeline_post" type="text" placeholder="5" value={lineThree} onChange={e=> setLineThree(e.target.value)}/> 
